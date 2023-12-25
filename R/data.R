11c78c9d724e037fdd29598bbc45ea88b3c2e66a
Daniel Koska <danielkoska86@gmail.com>
2023-12-25 16:46:02 +0100

    Added original R code in 'R' folder

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example data set containing time series (aka curve) data
+#'
+#' The data set consists of 110 curves (i.e, time series). The data set has a
+#' hierarchical structure: It contains 10 curves each from 11  subjects.
+#' All curves are of equal length (n = 101 curve points).
+#'
+#' @docType data
+#'
+#' @usage data(data)
+#'
+#' @format A data frame consisting of 11 rows and 110 columns. Each row
+#' represents a subject, each column a curve point.
+#'
+#' @keywords dataset
+"data"
