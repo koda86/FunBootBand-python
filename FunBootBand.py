51b2a81d9e182edc6a37ea639fbb7a7876cbae4b
Daniel Koska <danielkoska86@gmail.com>
2023-12-26 22:38:03 +0100

    Implemented check_arguments()

--- FunBootBand.py
+++ FunBootBand.py
@@ -1,12 +1,53 @@
 import pandas as pd
 import numpy as np
 
-def check_arguments(data, B, alpha, iid):
-    """
-    Function to check the validity of the input arguments.
-    """
-    # TODO: Implement argument checking logic
-    pass
+# def check_arguments(data, B, alpha, iid):
+def check_arguments(type, alpha, iid, k_coef, B, data):
+    # Function to check the validity of the input arguments.
+    if not isinstance(type, str):
+        raise ValueError("'type' must be a variable of type 'character'.")
+    elif type not in ["confidence", "prediction"]:
+        raise ValueError("'type' must be either 'confidence' or 'prediction'.")
+
+    if not isinstance(alpha, (int, float)) or alpha <= 0 or alpha >= 1:
+        raise ValueError("'alpha' must be a numeric value between 0 and 1.")
+
+    if not isinstance(iid, bool):
+        raise ValueError("'iid' must be a logical value (True or False).")
+
+    if not isinstance(k_coef, (int, float)) or k_coef <= 0:
+        raise ValueError("'k.coef' must be a positive integer.")
+
+    if not isinstance(B, (int, float)) or B <= 0:
+        raise ValueError("'B' must be a positive integer.")
+
+    if data.isna().any().any():
+        raise ValueError("Function stopped due to NA's in the input data.")
+      
+    return "All checks passed successfully."
+
+    
+    
+    # if (!inherits(type, "character")) {
+    #   stop("'type' must be a variable of type 'character'.")
+    # } else if (!(type %in% c("confidence", "prediction"))) {
+    #   stop("'type' must be either 'confidence' or 'prediction'.")
+    # }
+    # if (!is.numeric(alpha) || alpha <= 0 || alpha >= 1) {
+    #   stop("'alpha' must be a numeric value between 0 and 1.")
+    # }
+    # if (!is.logical(iid)) {
+    #   stop("'iid' must be a logical value (TRUE or FALSE).")
+    # }
+    # if (!is.numeric(k.coef) || k.coef <= 0) {
+    #   stop("'k.coef' must be a positive integer.")
+    # }
+    # if (!is.numeric(B) || B <= 0) {
+    #   stop("'B' must be a positive integer.")
+    # }
+    # if (any(is.na(data))) {
+    #   stop("Function stopped due to NA's in the input data.")
+    # }
 
 def approximate_curves(data):
     """
@@ -29,25 +70,25 @@ def construct_bands(bootstrap_samples, alpha):
     # TODO: Implement band construction logic
     pass
 
-def main(data):
-  # Assume 'data' is pandas data.frame
-  colnames = data.columns
-  return colnames
+def main(data, B=1000, alpha=0.05, iid=True):
+    """
+    Main function to calculate statistical bands.
+    """
+    # Check arguments
+    check_arguments(data, B, alpha, iid)
 
-# def main(data, B=1000, alpha=0.05, iid=True):
-#     """
-#     Main function to calculate statistical bands.
-#     """
-#     # Check arguments
-#     check_arguments(data, B, alpha, iid)
-# 
-#     # Approximate curves
-#     approximated_data = approximate_curves(data)
-# 
-#     # Bootstrap
-#     bootstrap_samples = bootstrap(approximated_data, B, iid)
-# 
-#     # Construct bands
-#     bands = construct_bands(bootstrap_samples, alpha)
-# 
-#     return bands
+    # Approximate curves
+    approximated_data = approximate_curves(data)
+
+    # Bootstrap
+    bootstrap_samples = bootstrap(approximated_data, B, iid)
+
+    # Construct bands
+    bands = construct_bands(bootstrap_samples, alpha)
+    
+    # Test if implementation works ...
+    # Assume 'data' is pandas data.frame
+    colnames = data.columns
+    bands = colnames
+
+    return bands
--- test_FunBootBand.py
+++ test_FunBootBand.py
@@ -7,9 +7,16 @@ import FunBootBand
 # Example usage
 os.chdir('/home/daniel/FunBootBand-python/')
 data = pd.read_csv('example_data.csv')
-result = FunBootBand.main(data)
 
-print(result)
+B = 5
+alpha = 0.05
+iid = True
+type = 'prediction'
+k_coef = 20
+
+print(FunBootBand.check_arguments(type, alpha, iid, k_coef, B, data))
+
+print(FunBootBand.main(data))
 
 # Need to be translated to Python!
 # plot(data[, 1], type = "l", ylim = c(-3, 3), ylab = "Amplitude")
